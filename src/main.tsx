d96c748c707714f974f40b09e4ccae0053297dfe
lacsy <olacomarkaidel@gmail.com>
2026-02-19 19:34:11 +0800

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
-import "./styles/auth.css";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </HashRouter>
+    <BrowserRouter basename="/VocalInkWEB-/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
